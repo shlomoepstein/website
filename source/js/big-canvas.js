d57cff2747b0a8eb29f79e6946f11d78e0d5b2c1
Shlomo <buoyant.puzzle0k@icloud.com>
2025-04-28 00:40:56 -0400

    .

--- source/js/big-canvas.js
+++ source/js/big-canvas.js
@@ -13,17 +13,19 @@ let getCoords = element => {
       y: scrollY + box.y,
       width: box.width,
       height: box.height,
-      centerX: scrollX + box.x + (box.width / 2),
-      centerY: scrollY + box.y + (box.height / 2)
+      centerX: scrollX + box.x +
+               (box.width / 2),
+      centerY: scrollY + box.y +
+               (box.height / 2)
    };
 };
 
 let scrollTo = (element, scrollBehavior) => {
    let coords = getCoords(element);
    let x = coords.centerX -
-              (innerWidth / 2),
+           (innerWidth / 2),
        y = coords.centerY -
-              (innerHeight / 2);
+           (innerHeight / 2);
    window.scrollTo({
       top: y,
       left: x,
@@ -33,46 +35,13 @@ let scrollTo = (element, scrollBehavior) => {
 
 
 window.addEventListener('load', () => {
+   let origin = document
+      .querySelector('#origin');
 
-let origin = document
-   .querySelector('#origin');
+   scrollTo(origin, 'instant');
 
-console.log(
-   origin.getBoundingClientRect());
-console.log(`\
-window.scrollX = ${scrollX}
-window.scrollY = ${scrollY}
-window.innerWidth = ${innerWidth}
-window.innerHeight = ${innerHeight}`
-);
-
-scrollTo(origin, 'instant');
-
-console.log(
-   origin.getBoundingClientRect());
-console.log(`\
-window.scrollX = ${scrollX}
-window.scrollY = ${scrollY}
-window.innerWidth = ${innerWidth}
-window.innerHeight = ${innerHeight}`
-);
-
-document
-   .querySelector('#jump-to-origin')
-   .addEventListener('click', () =>
-      scrollTo(origin, 'smooth'));
-
-document
-   .querySelector('#log-coords')
-   .addEventListener('click', () => {
-      console.log(
-         origin.getBoundingClientRect()
-      );
-      console.log(`\
-window.scrollX = ${scrollX}
-window.scrollY = ${scrollY}
-window.innerWidth = ${innerWidth}
-window.innerHeight = ${innerHeight}`
-      );
-   });
+   document
+      .querySelector('#jump-to-origin')
+      .addEventListener('click', () =>
+         scrollTo(origin, 'smooth'));
 });
