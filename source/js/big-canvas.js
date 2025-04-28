a8fedcb9929f67d200cc0c25348709b239ef21d5
Shlomo <buoyant.puzzle0k@icloud.com>
2025-04-27 20:32:51 -0400

    .

--- source/js/big-canvas.js
+++ source/js/big-canvas.js
@@ -5,9 +5,20 @@ let width = body.scrollWidth,
     vp_width = window.innerWidth,
     vp_height = window.innerHeight;
 
-document.querySelector('#jump-to-origin').onclick = () =>
-   window.scrollTo({
-      top: (height / 2) - (vp_height / 2),
-      left: (width / 2) - (vp_width / 2),
-      behavior: 'smooth'
-   });
+window.addEventListener(
+   'load',
+   () => {
+      document.querySelector(
+         '#jump-to-origin'
+      ).addEventListener(
+         'click',
+         () => {
+            window.scrollTo({
+               top: (height / 2) - (vp_height / 2),
+               left: (width / 2) - (vp_width / 2),
+               behavior: 'smooth'
+            })
+         }
+      )
+   }
+);
