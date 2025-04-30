af69d6842dc43e76232cbe61352939a2bef32666
Shlomo <buoyant.puzzle0k@icloud.com>
2025-04-30 03:11:11 -0400

    .

--- source/js/big-canvas.js
+++ source/js/big-canvas.js
@@ -3,18 +3,29 @@ let origin =
 let instant = {
    behavior: 'instant',
    block: 'center',
-   inline: 'center' };
+   inline: 'center'
+};
 let smooth = {
    behavior: 'smooth',
    block: 'center',
-   inline: 'center' };
+   inline: 'center'
+};
+
+let before = visualViewport.height;
 
 // after first layout and paint
 requestAnimationFrame(() =>
    requestAnimationFrame(() =>
       origin.scrollIntoView(instant)));
 
+let after = visualViewport.height;
+alert(`${before}\n${after}`)
+
 document
    .querySelector('#jump-to-origin')
-   .addEventListener('click', () =>
-      origin.scrollIntoView(smooth));
+   .addEventListener('click', () => {
+      origin.scrollIntoView(smooth);
+      alert(
+         `${visualViewport.height}`
+      );
+   });
