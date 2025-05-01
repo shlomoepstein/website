3f2e02782cd800f8cf422ce755f2e6ba99055d9d
Shlomo <buoyant.puzzle0k@icloud.com>
2025-05-01 13:34:50 -0400

    .

--- source/js/big-canvas.js
+++ source/js/big-canvas.js
@@ -14,7 +14,12 @@ let delay = ms =>
    new Promise(resolve =>
       setTimeout(resolve, ms));
 
-
+// (mostly) fixes a bug where scrollIntoView
+// sometimes scrolls a little too low on the
+// first load, (but not every time); couldn't
+// figure it out, this is a hacky workaround.
+// (20ms seems to work, but there's nothing
+// special about that number.)
 (async () => {
    await delay(20);
    requestAnimationFrame(() =>
